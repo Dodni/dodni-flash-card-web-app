d90a2f44e4e9be0cbdc61f5c09b1931c703d6399
Donát Fehér <50624191+Dodni@users.noreply.github.com>
2024-03-07 00:39:48 +0100

    adding login C a V

---
+++ app/controllers/login_controller.php
@@ -0,0 +1,22 @@
+<?php
+class LoginController {
+    public function showLoginPage() {
+        $viewPath = 'app/views/login_view.php';
+        
+        // Ellenőrizzük, hogy a fájl létezik-e
+        if (file_exists($viewPath)) {
+            // Betöltjük és megjelenítjük a nézetet
+            include_once $viewPath;
+        } else {
+            echo "A megadott nézetfájl nem található.";
+        }
+        
+    }
+}
+
+$controller = new LoginController();
+
+$controller->showLoginPage();
+
+
+?>
