eaebea6d2c4d6b57745c7487768dd38692cd41cc
Donát Fehér <50624191+Dodni@users.noreply.github.com>
2024-03-07 00:40:17 +0100

    menu and title update

--- app/views/home_view.php
+++ app/views/home_view.php
@@ -6,9 +6,11 @@
 </head>
 <?php include_once 'header_view.php'; ?>
 <body>
-    <h1>Home page</h1>
+    <h1>Dodni Flash Card Web App</h1>
     <a href="<?php echo BASE_URL;?>about">About</a><br>
-    <a href="<?php echo BASE_URL;?>contact">Contact</a>
+    <a href="<?php echo BASE_URL;?>contact">Contact</a><br>
+    <a href="<?php echo BASE_URL;?>register">Register</a><br>
+    <a href="<?php echo BASE_URL;?>login">Login</a>
 </body>
 <?php include_once 'footer_view.php'; ?>
 </html>
