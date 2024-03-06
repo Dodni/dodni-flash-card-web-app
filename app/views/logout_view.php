f62b1607412d1fdfae6b58096c8e2fc256e03580
Donát Fehér <50624191+Dodni@users.noreply.github.com>
2024-03-07 00:58:38 +0100

    adding new views

--- app/views/logout_view.php
+++ app/views/logout_view.php
@@ -2,11 +2,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Register</title>
+    <title>Logged out</title>
 </head>
 <?php include_once 'header_view.php'; ?>
 <body>
-    <h1>Register</h1>
+    <h1>Logged out</h1>
     <a href="<?php echo BASE_URL;?>home">Home</a><br>
     <a href="<?php echo BASE_URL;?>about">About</a><br>
     <a href="<?php echo BASE_URL;?>contact">Contact</a><br>
