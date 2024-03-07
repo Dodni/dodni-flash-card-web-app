d13dfeab27f19ee9348325da97528ee92af8ac52
Donát Fehér <50624191+Dodni@users.noreply.github.com>
2024-03-07 10:21:20 +0100

    added dark theme

--- app/views/en/contact_view.php
+++ app/views/en/contact_view.php
@@ -3,6 +3,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Contact</title>
+    <link rel="stylesheet" href="public/css/main_style.css">
 </head>
 <?php include_once 'header_view.php'; ?>
 <body>
@@ -12,7 +13,21 @@
     <a href="<?php echo BASE_URL;?>contact">Contact</a><br>
     <a href="<?php echo BASE_URL;?>register">Register</a><br>
     <a href="<?php echo BASE_URL;?>login">Login</a><br>
-    <p>Coming soon..</p>
+    <div class="contact_container">
+        <h2>Contact Us</h2>
+        <form action="submit.php" method="post">
+            <label for="contact_name">Name:</label>
+            <input type="text" id="contact_name" name="contact_name" required>
+
+            <label for="contact_email">Email:</label>
+            <input type="email" id="contact_email" name="contact_email" required>
+
+            <label for="contact_message">Message:</label>
+            <textarea id="contact_message" name="contact_message" rows="4" required></textarea>
+
+            <button type="submit">Send Message</button>
+        </form>
+    </div>
 </body>
 <?php include_once 'footer_view.php'; ?>
 </html>
--- app/views/en/header_view.php
+++ app/views/en/header_view.php
@@ -1,8 +1,29 @@
 <header>
+    <script>
+        function toggleTheme() {
+            var body = document.body;
+            var sunIcon = document.getElementById("sunIcon");
+            var moonIcon = document.getElementById("moonIcon");
+
+            if (body.classList.contains("dark-theme")) {
+                body.classList.remove("dark-theme");
+                sunIcon.style.display = "block";
+                moonIcon.style.display = "none";
+            } else {
+                body.classList.add("dark-theme");
+                sunIcon.style.display = "none";
+                moonIcon.style.display = "block";
+            }
+        }
+    </script>
     <form id="languageForm" action="submit.php" method="post">
         <select name="language" id="languageSelect" onchange="submitForm()">
             <option value="EN">EN</option>
             <option value="HU">HU</option>
         </select>
     </form>
+    <button onclick="toggleTheme()">
+        <img id="sunIcon" src="sun_icon.png" alt="☼" style="display: block;">
+        <img id="moonIcon" src="moon_icon.png" alt="☾" style="display: none;">
+    </button>
 </header>
