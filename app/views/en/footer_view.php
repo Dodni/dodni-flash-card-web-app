ade2a1139cde60dcf6ef92dfdd6f48fadf1187d2
Donát Fehér <50624191+Dodni@users.noreply.github.com>
2024-03-07 11:51:03 +0100

    Update footer_view.php

--- app/views/en/footer_view.php
+++ app/views/en/footer_view.php
@@ -1,5 +1,8 @@
 <!-- footer.php -->
 <footer>
-    <p>Footer</p>
+    <p>All rights reserved &copy; <span id="currentYear"></span> Dodni's Flash Card Web App</p>
     <script src="app/js/main.js"></script>
+    <script>
+    document.getElementById("currentYear").textContent = new Date().getFullYear();
+    </script>
 </footer>
