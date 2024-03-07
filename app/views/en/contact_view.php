0128cd2aece92abeca5c3f31347df28f9194ba17
Donát Fehér <50624191+Dodni@users.noreply.github.com>
2024-03-07 11:28:17 +0100

    setted up the new head-main_view

--- app/views/en/contact_view.php
+++ app/views/en/contact_view.php
@@ -1,10 +1,8 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php include_once 'head-main_view.php'; ?>
     <title>Contact</title>
-    <link rel="stylesheet" href="public/css/main_style.css">
 </head>
 <?php include_once 'header_view.php'; ?>
 <body>
--- app/views/en/deck_view.php
+++ app/views/en/deck_view.php
@@ -1,8 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php include_once 'head-main_view.php'; ?>
     <title>Deck's name here</title>
 </head>
 <?php include_once 'header_view.php'; ?>
