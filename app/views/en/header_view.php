aecc8bf503a164f37bbeea85d7212348d68e9dae
Donát Fehér <50624191+Dodni@users.noreply.github.com>
2024-03-07 11:29:01 +0100

    edited the toggleTheme

--- app/views/en/header_view.php
+++ app/views/en/header_view.php
@@ -1,12 +1,9 @@
 <header>
-    <form id="languageForm" action="submit.php" method="post">
+    <form id="languageForm" action="submit.php" method="post" class="header-item">
         <select name="language" id="languageSelect" onchange="submitForm()">
             <option value="EN">EN</option>
             <option value="HU">HU</option>
         </select>
     </form>
-    <button onclick="toggleTheme()">
-        <img id="sunIcon" src="sun_icon.png" alt="☼" style="display: block;">
-        <img id="moonIcon" src="moon_icon.png" alt="☾" style="display: none;">
-    </button>
+    <button id="themeButton" onclick="toggleTheme()" class="header-item">☼</button>
 </header>
