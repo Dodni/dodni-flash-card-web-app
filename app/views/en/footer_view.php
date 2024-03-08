c8f6abbfef10e166886008f66853837ae25d9d0c
Donát Fehér <50624191+Dodni@users.noreply.github.com>
2024-03-08 23:11:43 +0100

    Update footer_view.php

--- app/views/en/footer_view.php
+++ app/views/en/footer_view.php
@@ -3,7 +3,5 @@
     <p>All rights reserved &copy; <span id="currentYear"></span> Dodni's Flash Card Web App</p>
     <script src="<?php echo BASE_URL; ?>app/js/main.js"></script>
     <script src="<?php echo BASE_URL; ?>app/js/card-flipping.js"></script>
-    <script>
-        document.getElementById("currentYear").textContent = new Date().getFullYear();
-    </script>
+    <script>document.getElementById("currentYear").textContent = new Date().getFullYear();</script>
 </footer>
