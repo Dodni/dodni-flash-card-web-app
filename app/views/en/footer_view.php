9309980bc0c8273334eaae60697770efa22d3f2f
Donát Fehér <50624191+Dodni@users.noreply.github.com>
2024-03-08 15:50:01 +0100

    base url added up to the links

--- app/views/en/footer_view.php
+++ app/views/en/footer_view.php
@@ -1,8 +1,9 @@
 <!-- footer.php -->
 <footer>
     <p>All rights reserved &copy; <span id="currentYear"></span> Dodni's Flash Card Web App</p>
-    <script src="app/js/main.js"></script>
+    <script src="<?php echo BASE_URL; ?>app/js/main.js"></script>
+    <script src="<?php echo BASE_URL; ?>app/js/card-flipping.js"></script>
     <script>
-    document.getElementById("currentYear").textContent = new Date().getFullYear();
+        document.getElementById("currentYear").textContent = new Date().getFullYear();
     </script>
 </footer>
--- app/views/en/head-main_view.php
+++ app/views/en/head-main_view.php
@@ -1,3 +1,3 @@
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<link rel="stylesheet" href="public/css/main_stlye.css">
+<link rel="stylesheet" href="<?php echo BASE_URL; ?>public/css/main_style.css">
