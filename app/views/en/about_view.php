c38d43c3e296d65319a9c543e2c8604716a9f8f8
Donát Fehér <50624191+Dodni@users.noreply.github.com>
2024-03-09 13:52:33 +0100

    Update about_view.php

--- app/views/en/about_view.php
+++ app/views/en/about_view.php
@@ -8,7 +8,9 @@
         <?php include_once 'header_view.php'; ?>
         <main>
         <h1>About Us</h1>
-            <p class="centered container">Our website is a flash card web application. It is free to use. The name is got from the creater by Dodni.</p>
+            <p class="centered container">Our website is a flash card web application. Learn faster than 
+                everyone else with the technology.</p>
+            <p class="centered container">The name is got from the creator by Dodni.</p>
         </main>
         <?php include_once 'footer_view.php'; ?>
     </body>
