809ca7762190f108654e5ef9e69685fda37c79f7
Donát Fehér <50624191+Dodni@users.noreply.github.com>
2024-03-14 16:39:10 +0100

    sign up C update, data check done

--- app/controllers/signup_controller.php
+++ app/controllers/signup_controller.php
@@ -12,7 +12,39 @@ class SignUpController {
 }
 
 $controller = new SignUpController();
+var_dump ($_POST);
+if($_SERVER["REQUEST_METHOD"] == 'POST' 
+    && isset($_POST['username'])
+    && isset($_POST['email'])
+    && isset($_POST['password'])
+    && isset($_POST['termsCheckbox'])) {
+    echo "itt vagyunk";
 
-$controller->showSignUpPage();
+    // Data check:
+    // Felhasználónév ellenőrzése
+    if (!isset($_POST['username']) || empty($_POST['username'])) {
+        echo "Felhasználónév mező kitöltése kötelező.";
+    }
+
+    // Email ellenőrzése
+    if (!isset($_POST['email']) || empty($_POST['email'])) {
+        echo "Email mező kitöltése kötelező.";
+    } elseif (!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+        echo "Érvénytelen email formátum.";
+    }
+
+    // Jelszó ellenőrzése
+    if (!isset($_POST['password']) || empty($_POST['password'])) {
+        echo "Jelszó mező kitöltése kötelező.";
+    }
+
+    // Checkbox ellenőrzése (általában csak akkor van szükség erre, ha a checkbox csak akkor van bejelölve, ha kiválasztják)
+    if (!isset($_POST['termsCheckbox']) || $_POST['termsCheckbox'] !== 'on') {
+        echo "A felhasználási feltételek elfogadása kötelező.";
+    }
+
+} else {
+    $controller->showSignUpPage();
+}
 
 ?>
