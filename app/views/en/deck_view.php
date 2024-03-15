040f5360f07a807f600b0c94e870e44a24ec55af
Donát Fehér <50624191+Dodni@users.noreply.github.com>
2024-03-15 18:38:39 +0100

    deck view deigne updated

--- app/views/en/deck_view.php
+++ app/views/en/deck_view.php
@@ -43,19 +43,19 @@
                         <input type="hidden" id="cardsNumberForTheSession" name="cardsNumberForTheSession" value="">
                         
                         <input type="checkbox" class="card-checkbox" id="all" name="options[]" value="0" onchange="updateKnownCardsNumber()">
-                        <label for="all" class="card-label">NEW (<?php echo $knownCardsNumber[0]?>)</label>
+                        <label for="all" class="card-label-new">NEW (<?php echo $knownCardsNumber[0]?>)</label>
                         
                         <input type="checkbox" class="card-checkbox" id="fail" name="options[]" value="1" onchange="updateKnownCardsNumber()">
-                        <label for="fail" class="card-label">FAIL (<?php echo $knownCardsNumber[1]?>)</label>
+                        <label for="fail" class="card-label-fail">FAIL (<?php echo $knownCardsNumber[1]?>)</label>
                         
                         <input type="checkbox" class="card-checkbox" id="bad" name="options[]" value="2" onchange="updateKnownCardsNumber()">
-                        <label for="bad" class="card-label">HARD (<?php echo $knownCardsNumber[2]?>)</label>
+                        <label for="bad" class="card-label-hard">HARD (<?php echo $knownCardsNumber[2]?>)</label>
                         
                         <input type="checkbox" class="card-checkbox" id="good" name="options[]" value="3" onchange="updateKnownCardsNumber()">
-                        <label for="good" class="card-label">GOOD (<?php echo $knownCardsNumber[3]?>)</label>
+                        <label for="good" class="card-label-good">GOOD (<?php echo $knownCardsNumber[3]?>)</label>
                         
                         <input type="checkbox" class="card-checkbox" id="easy" name="options[]" value="4" onchange="updateKnownCardsNumber()">
-                        <label for="easy" class="card-label">EASY (<?php echo $knownCardsNumber[4]?>)</label>
+                        <label for="easy" class="card-label-easy">EASY (<?php echo $knownCardsNumber[4]?>)</label>
                     </div>
                     <div class="container-fluid ">
                         <input class="flip-button fixed-bottom" type="submit" id="submitButton" value="Start" disabled>
