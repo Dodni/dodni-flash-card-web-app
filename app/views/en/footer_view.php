ae48bac44aecd9c0ea721770a0dd7ae0edbaf25b
Donát Fehér <50624191+Dodni@users.noreply.github.com>
2024-03-17 10:29:50 +0100

    error fixed

--- app/views/en/footer_view.php
+++ app/views/en/footer_view.php
@@ -4,7 +4,6 @@
      -->
     <script src="<?php echo BASE_URL; ?>app/js/main.js"></script>
     <script src="<?php echo BASE_URL; ?>app/js/card-flipping.js"></script>
-    <script>document.getElementById("currentYear").textContent = new Date().getFullYear();</script>
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.3/dist/umd/popper.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
