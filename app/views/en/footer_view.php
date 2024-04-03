0be6520e100b5981f4d68657792c23928eba203e
Donát Fehér <50624191+Dodni@users.noreply.github.com>
2024-04-03 13:56:51 +0200

    sources stored now in the project
    sources stored now in the project and we stopped download them always by links

--- app/views/en/footer_view.php
+++ app/views/en/footer_view.php
@@ -4,7 +4,7 @@
      -->
     <script src="<?php echo BASE_URL; ?>app/js/main.js"></script>
     <script src="<?php echo BASE_URL; ?>app/js/card-flipping.js"></script>
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.3/dist/umd/popper.min.js"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <script src="<?php echo BASE_URL; ?>app/js/popper.min.js"></script>
+    <script src="<?php echo BASE_URL; ?>app/js/jquery.slim.min.js"></script>
+    <script src="<?php echo BASE_URL; ?>app/js/bootstrap.min.js"></script>
 </footer>
--- app/views/en/head-main_view.php
+++ app/views/en/head-main_view.php
@@ -1,4 +1,4 @@
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <link rel="stylesheet" href="<?php echo BASE_URL; ?>public/css/main_style.css">
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+<link rel="stylesheet" href="<?php echo BASE_URL; ?>public/css/bootstrap.min.css">
