67f736940dfc1c74d35623dab210adeb81da2d63
Donát Fehér <50624191+Dodni@users.noreply.github.com>
2024-04-07 17:56:19 +0200

    Update about_view.php

--- app/views/en/about_view.php
+++ app/views/en/about_view.php
@@ -8,11 +8,12 @@
         <?php include_once 'header_view.php'; ?>
         <main>
             <div class="container text-center mt-2">
-                <h1>About the Dodni's Flash Card World!</h1>
+                <h1>About the Dodni's Flash Card World Application!</h1>
                 <div class="container mt-2 col-md-8">
                     <p>We are <span style="color: #449DD1;">passionate</span> about revolutionizing the way people learn through our online flashcard application.</p>
                     <p>Our flashcard app offers a <span style="color: #449DD1;">dynamic</span> and <span style="color: #449DD1;">engaging</span> way to learn new material efficiently.</p>
                     <p>With features like <span style="color: #449DD1;">convenience</span>, <span style="color: #449DD1;">customization</span>, and <span style="color: #449DD1;">engagement</span>, we aim to make learning fun and effective.</p>
+                    <p>There are around <span style="color: #449DD1;">200 languages</span> available. Include <span style="color: #449DD1;">your Decks</span> on the website, or find one in the <span style="color: #449DD1;">Public Decks</span> and start your journey.</p>
                     <p>Join us today and discover the <span style="color: #449DD1;">power</span> of online flashcards!</p>
                 </div>
                 <div class="container mt-2 col-md-8">
